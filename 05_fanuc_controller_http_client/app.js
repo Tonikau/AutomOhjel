6edbbf67e4b64fe6aa410e164541465c00b056fc
Tonikau <toni.kaunismaki@gmail.com>
2022-01-17 14:25:17 +0200

    harjoitukset valmiit

---
+++ 05_fanuc_controller_http_client/app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const axios = require('axios');
+const { json } = require('express');
+
+const PORT = process.env.PORT || 8081;
+
+let app = express();
+
+
+app.use((req, res, next) => {
+    console.log('PATH: ' + req.path);
+    next();
+});
+
+app.get('/', (req, res, next) => {
+    res.send('Hello World');
+    res.end();
+});
+
+
+
+app.get("/robot/joint_values/", (req, joints, next) => {
+    axios.get('https://fanuc-robot-http-server.herokuapp.com/').then((res) => {
+        const regexp = 'Joint   [1-6]: *(-?.*)'; //[1-6]
+         let joint_values = [];
+         let matches = res.data.matchAll(regexp);
+         let count = 0;
+            for (const match of matches)
+            {
+            count++;
+            if (count > 6) break;
+            const value = parseFloat(match[1]);
+            joint_values.push(value);
+            };
+        joints.send( {joint_values});
+        console.log({joint_values});
+    });
+});
+    
+
+  app.listen(PORT);
+
+
+
+
+// app.get("/robot/joint_values/", (req, res, next) => {
+//     axios.get('https://fanuc-robot-http-server.herokuapp.com/').then((res)=>{
+//             const regexp = 'Joint   [1-6]: *(-?.*)'; //[1-6]
+//             let joint_values = [];
+//             let matches = res.data.matchAll(regexp);
+//             let count = 0;
+             
+//             for (const match of matches) {
+//                  count++;
+//                  if (count > 6) break;
+//                 const value = parseFloat(match[1]);
+//                 joint_values.push(value); 
+//             }
+            
+//             });
+//             // res.send({joint_values});
+            
+        
+//         console.log({joint_values});
+//     app.listen(PORT);
